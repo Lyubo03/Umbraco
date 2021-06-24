bf9ffb1c929d4e1f9e9677a173d76b97dacc217a
Lyuboslav <Lyuboslav.Balev@klevret.com>
2021-06-24 18:36:48 +0300

    Delete Article Implemented

--- HighlyDeveloped.Core/Controllers/EditArticleController.cs
+++ HighlyDeveloped.Core/Controllers/EditArticleController.cs
@@ -1,12 +1,28 @@
 ﻿namespace HighlyDeveloped.Core.Controllers
 {
     using System.Web.Mvc;
+    using Umbraco.Core.Services;
     using Umbraco.Web.Mvc;
-
     public class EditArticleController : SurfaceController
     {
-        public ActionResult DeleteArticle()
+        private readonly IContentService contentService;
+
+        public EditArticleController(IContentService contentService)
         {
+            this.contentService = contentService;
+        }
+
+        [HttpPost]
+        public ActionResult DeleteArticle(int id)
+        {
+            var content = contentService.GetById(id);
+
+            if (content != null)
+            {
+                contentService.Delete(content);
+
+                return Redirect("/");
+            }
             return RedirectToCurrentUmbracoPage();
         }
     }
